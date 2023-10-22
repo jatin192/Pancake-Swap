27e3c8b24fa6170bc2b0862c7b8c50df64862b4d
Jatin <73174196+jatin192@users.noreply.github.com>
2023-10-22 21:59:26 +0530

    Add files via upload

---
+++ Address_List.js
@@ -0,0 +1,7 @@
+//contract addresses
+let Pancake_Factory_address = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"; //factory pancake address ->Search on bscscan
+let Pancake_Rounter_address ="0x10ED43C718714eb63d5aA57B78B54704E256024E";  //Router pancake address
+let from_address="0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";//BUSD token address
+let to_address ="0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";//WBNB token address
+
+module.exports = { Pancake_Factory_address,Pancake_Rounter_address,from_address,to_address}
